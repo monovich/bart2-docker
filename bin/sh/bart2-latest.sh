cdd1844534a1ec24347c2bffd173b67a9e6dd02f
Alexander Monovich <monovich@umich.edu>
2020-10-29 08:16:58 -0400

    Update bart2-latest.sh

--- bin/sh/bart2-latest.sh
+++ bin/sh/bart2-latest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -v "$BART/data/":/home/BARTv2.0/data/ -v "$BART/bin/":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ bart2:latest /bin/bash
+docker run --rm -ti -v "$BART/data/":/home/BARTv2.0/data/ -v "$BART/bin/":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ monovich/bart2:latest /bin/bash
