efda96c6300f77e172afa55ea736e69bbfeea1eb
Alexander C. Monovich <monovich@umich.edu>
2020-10-29 08:07:37 -0400

    fixed scripts

--- bin/sh/bart2-1.0.sh
+++ bin/sh/bart2-1.0.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-repo=$(head -n 1 ./path.txt)
-docker run --rm -ti -v "$repo/data":/home/BARTv2.0/data/ -v "$repo/bin":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ bart2:1.0 /bin/bash
+docker run --rm -ti -v "$BART/data":/home/BARTv2.0/data/ -v "$BART/bin":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ bart2:1.0 /bin/bash
