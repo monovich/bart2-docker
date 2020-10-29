e7983fcc1d26f12435b2ae13e7ab61c698dfd6cf
Alexander Monovich <monovich@umich.edu>
2020-10-29 07:33:37 -0400

    Update bart2-1.0.sh

--- bin/sh/bart2-1.0.sh
+++ bin/sh/bart2-1.0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 repo=$(head -n 1 ./path.txt)
-docker run --rm -ti -v "$repo/data":/home/BARTv2.0/data/ -v "$repo/bin":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ bart2:1.0 /bin/bash
+docker run --rm -ti -v "$repo/data":/home/BARTv2.0/data/ -v "$repo/bin":/home/BARTv2.0/bin/ -w /home/BARTv2.0/ monovich/bart2:1.0 /bin/bash
